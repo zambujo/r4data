dc2bee82ca2a772621049bffe1f10184cf4e9f9f
João Martins <joao.martins@snf.ch>
2021-04-25 11:30:00 +0200

    ⚗️  Adding cross-referencing between institutes
    Not very practical

--- install.R
+++ install.R
@@ -1,7 +1,13 @@
 `%>%` <- magrittr::`%>%`
 usethis::ui_info("updating data ....")
-
 usethis::ui_done("deploy site ...")
+
+people <- readr::read_csv(here("data", "people.csv")) #  p3 people data on r4d
+institute_numbers <- people %>%
+  dplyr::filter(!is.na(institute_id_p3)) %>%
+  dplyr::distinct(institute_id_p3) %>%
+  dplyr::pull(institute_id_p3)
+
 here::here("Rmd", "explore.Rmd") %>%
   rmarkdown::render(
     output_format = "flexdashboard::flex_dashboard",
@@ -9,3 +15,16 @@ here::here("Rmd", "explore.Rmd") %>%
     output_dir = here::here("docs"),
     quiet = FALSE
   )
+
+render_location <- function(x) {
+  here::here("Rmd", "explore.Rmd") %>%
+    rmarkdown::render(
+      output_format = "flexdashboard::flex_dashboard",
+      output_file = glue::glue("{x}.html"),
+      output_dir = here::here("docs"),
+      quiet = FALSE,
+      params = list(inst_id = x)
+    )
+}
+
+purrr::walk(institute_numbers, render_location)
